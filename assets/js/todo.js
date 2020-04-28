c597c67ceb0db57b93c8a04a8594680ef64ff086
João Vitor Dutra <jo.vitor.d@gmail.com>
2020-04-28 18:53:43 -0300

    Demonstração forEach JavaScript

---
+++ assets/js/todo.js
@@ -0,0 +1,30 @@
+// Array de JSON
+let data = [
+  {
+    id: 1,
+    title: "Estudar HTML",
+  },
+  {
+    id: 2,
+    title: "Estudar CSS",
+  },
+  {
+    id: 3,
+    title: "Estudar JavaScript",
+  },
+  {
+    id: 4,
+    title: "Estudar PHP 7",
+  },
+];
+
+data.forEach((task) => {
+  let li = document.createElement("li");
+
+  li.innerHTML = `
+  <input type="checkbox" id="task-${task.id}" />
+  <label for="task-${task.id}">${task.title}</label>
+  `;
+
+  document.querySelector(".todo").append(li);
+});
